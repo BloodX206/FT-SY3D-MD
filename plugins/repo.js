712f90bc7395585493d6eefa713e454f32cd2335
BloodX206 <ayankashkali206@gmail.com>
2025-08-25 21:58:01 +0500

    Update repo.js

--- plugins/repo.js
+++ plugins/repo.js
@@ -11,7 +11,7 @@ cmd({
     filename: __filename,
 },
 async (conn, mek, m, { from, reply }) => {
-    const githubRepoURL = 'https://github.com/waqa827/E3_HACKER-MD';
+    const githubRepoURL = 'https://github.com/BloodX206/FT-SY3D-MD';
 
     try {
         const [, username, repoName] = githubRepoURL.match(/github\.com\/([^/]+)\/([^/]+)/);
@@ -172,15 +172,15 @@ ${repoData.description || 'No description provided'}
 
         // Send image with repo info
         await conn.sendMessage(from, {
-            image: { url: config.MENU_IMAGE_URL || 'https://files.catbox.moe/yj7zp0.png' },
+            image: { url: config.MENU_IMAGE_URL || 'https://files.catbox.moe/5tv7m5.jpg' },
             caption: selectedStyle,
             contextInfo: { 
                 mentionedJid: [m.sender],
                 forwardingScore: 999,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363318478753709@newsletter',
-                    newsletterName: config.OWNER_NAME || 'E3_HACKER_𝐌𝐃',
+                    newsletterJid: '120363421815468294@newsletter',
+                    newsletterName: config.OWNER_NAME || 'SYED_MD',
                     serverMessageId: 143
                 }
             }
@@ -188,7 +188,7 @@ ${repoData.description || 'No description provided'}
 
         // Send audio
         await conn.sendMessage(from, {
-            audio: { url: 'https://files.catbox.moe/vux548.mp4' },
+            audio: { url: 'https://files.catbox.moe/5tv7m5.jpg' },
             mimetype: 'audio/mp4',
             ptt: true,
             contextInfo: { 
